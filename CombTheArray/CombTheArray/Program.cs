98b535369e1f56448c472ef1f9e6ca8b582bb790
PiuPick <pticyn.leonid@mail.ru>
2024-03-26 22:18:32 +0300

    выполнил задание

---
+++ CombTheArray/CombTheArray/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace CombTheArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter arr size = ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[size];
+
+            Console.WriteLine("Enter " + size + " numbers:");
+            for (int i = 0; i < size; i++) arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            //int[] arr = { -6, -2, -3, 0, 6, 1, 3, 11, 11, 14 };
+            //int[] arr = { 22, 20, 18, 14, 10 };
+
+            int[] res = CombTheArray(arr);
+
+            foreach (int i in res)
+                Console.WriteLine(i);
+        }
+
+        static int[] CombTheArray(int[] arr)
+        {
+            int[] res = new int[arr.Length];
+            int temp = res[0] = arr[0];
+            int index = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] >= temp)
+                    res[++index] = temp = arr[i];
+            }
+
+            res = res[0..++index];
+
+            return res;
+        }
+    }
+}
---
+++ NLetterWords/NLetterWords/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace NLetterWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter the length of the word N\nN = ");
+            //int N_num= Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter the length of the alphabet used for the word M\nM = ");
+            //int M_num = Convert.ToInt32(Console.ReadLine());
+
+            NLetterWords(4, 3);
+
+            Console.WriteLine();
+
+            //NLetterWords_recursion(N_num, M_num);
+        }
+
+        const string alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+        static void NLetterWords(int N_num, int M_num)
+        {
+            string mini_alphabet = alphabet[..M_num]; // записали алфавит из M_num букв русского алфавита
+            int count_placements = (int)Math.Pow(M_num, N_num); // число размещений с повторениями
+
+            int[,] result = new int[count_placements, N_num];
+
+            int temp = 0;
+
+            for (int i = 1; i < count_placements; i++) // проходимся по каждому слову
+            {
+                result[i - 1, 0] = temp++;
+
+                for (int j = 1; j < N_num; j++) // проходимся по каждому символу (начиная со 2 по счету)
+                {
+                    result[i, j] = result[i - 1, j]; // копируем предыдущее слово
+                }
+
+                if (temp == M_num)
+                {
+                    temp = 0;
+                    int help = 1;
+
+                    while (help + 1 < N_num)
+                    {
+                        if (++result[i, help] == M_num)
+                        {
+                            result[i, help] = 0;
+                            ++result[i, ++help];
+                        }
+                        else break;
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < count_placements; i++)
+            {
+                for (int j = 0; j < N_num; j++)
+                {
+                    Console.Write(result[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            // Console.WriteLine(result);
+        }
+        static void NLetterWords_recursion(int N_num, int M_num)
+        {
+
+        }
+    }
+}
